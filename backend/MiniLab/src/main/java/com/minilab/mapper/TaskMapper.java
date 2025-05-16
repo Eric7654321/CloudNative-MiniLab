4035e225e0456918a80b0cf4bf2a386cbd5af9af
Eric7654321 <he888hello22@gmail.com>
2025-05-16 22:10:36 +0800

    implement everything except task-schedule

--- backend/MiniLab/src/main/java/com/minilab/mapper/TaskMapper.java
+++ backend/MiniLab/src/main/java/com/minilab/mapper/TaskMapper.java
@@ -1,13 +1,27 @@
 package com.minilab.mapper;
 
 import com.minilab.pojo.entity.Task;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
 public interface TaskMapper {
     @Select("select * from minilab.task where `group` = #{groupId}")
     List<Task> getTaskByGroupId(String groupId);
+
+    @Select("select * from minilab.task where emp = #{id} and start_time >= #{start} and start_time <= #{end}")
+    List<Task> getTaskByIdAndTime(Integer id, LocalDate start, LocalDate end);
+
+    @Update("update minilab.task set is_finish = 1, update_time = now() where id = #{taskId}")
+    void solveTask(Integer taskId);
+
+    @Delete("delete from minilab.task where id = #{id}")
+    void deleteTaskById(Integer id);
+
+    void updateTask(Task task);
 }
--- backend/MiniLab/src/main/java/com/minilab/service/impl/TaskServiceImpl.java
+++ backend/MiniLab/src/main/java/com/minilab/service/impl/TaskServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -20,5 +22,30 @@ public class TaskServiceImpl implements TaskService {
         List<Task> tasks = taskMapper.getTaskByGroupId(groupId);
         log.info("已查詢到tasks: {}, groupId={}", tasks, groupId);
         return tasks;
+    }
+
+    @Override
+    public List<Task> getTaskByIdAndTime(Integer id, LocalDate now) {
+        List<Task> tasks = taskMapper.getTaskByIdAndTime(id, now, now);
+        log.info("已查詢到tasks: {}, id= {}, day= {}", tasks, id);
+        return List.of();
+    }
+
+    @Override
+    public List<Task> getTaskByIdAndTimeWeeks(Integer id, LocalDate now) {
+        List<Task> tasks = taskMapper.getTaskByIdAndTime(id, now, now.plusDays(14));
+        log.info("已查詢到兩個禮拜內tasks: {}, id= {}, day= {}", tasks, id);
+        return List.of();
+    }
+
+    @Override
+    public void deleteTask(Task task) {
+        taskMapper.deleteTaskById(task.getId());
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        task.setUpdateTime(LocalDateTime.now());
+        taskMapper.updateTask(task);
     }
 }
