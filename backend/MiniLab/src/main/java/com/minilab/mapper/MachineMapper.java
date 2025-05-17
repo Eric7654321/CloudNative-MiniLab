aea97344aad098bbe9cbda44f9f8999934041b4d
Eric7654321 <he888hello22@gmail.com>
2025-05-17 16:08:13 +0800

    完成所有API/寫了一個unit test

--- backend/MiniLab/src/main/java/com/minilab/mapper/MachineMapper.java
+++ backend/MiniLab/src/main/java/com/minilab/mapper/MachineMapper.java
@@ -3,10 +3,7 @@ package com.minilab.mapper;
 import com.minilab.pojo.entity.Machine;
 import com.minilab.pojo.vo.EmpVO;
 import com.minilab.pojo.vo.MachineVO;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -15,6 +12,11 @@ public interface MachineMapper {
 
     @Select("select * from minilab.machine where `group` = #{groupId}")
     List<MachineVO> getMachineByGroupId(String groupId);
+
+    @Select("select * from minilab.machine where id = #{id}")
+    Machine getMachineById(Integer id);
+
+    List<Machine> getMachinesByIds(@Param("ids") List<Integer> ids);
 
     @Select("select tags from minilab.machine_tag where machine_id = #{id}")
     String setTagsByUserId(Integer id);
