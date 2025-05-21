c7d2cdfcdbcf564f3e71439bb7bfe8c65b78e14d
Welly17223 <144933318+Welly17223@users.noreply.github.com>
2025-05-21 10:19:05 +0800

    Feat/frontend (#5)
    * Update README.md
    
    * Update README.md
    
    * login return EmpVO
    
    * make frontend easier by /login return EmpVO
    
    * make EmpVO normal
    
    * Observability
    
    * add basic search API (emp/machine/task)
    
    * add API description
    
    * vue init
    
    * vue init
    
    * add ToDoItem component and implement basic to-do list functionality
    
    * add ToDoItem component and implement basic to-do list functionality
    
    * [init]: init the frontend
    
    * [init]: init the frontend
    
    * [env]: add package-lock.json
    
    * [env]: add package-lock.json
    
    * re init frontend
    
    * re init frontend
    
    * re init frontend in ts
    
    * re init frontend in ts
    
    * ts
    
    * ts
    
    * [feat] login system, not include page navigate
    
    * [feat] login system, not include page navigate
    
    * [fix] add env variable to connect to backend
    
    * [fix] add env variable to connect to backend
    
    * add head bar and work list
    
    * add head bar and work list
    
    * worklist get func
    
    * worklist get func
    
    * [edit] add route
    
    * [edit] add route
    
    * [fix] login reverse proxy fix
    
    * [fix] login reverse proxy fix
    
    * [fix] package-lock and package.json work
    
    * [fix] package-lock and package.json work
    
    * Add cookies and view redirect
    
    * Add cookies and view redirect
    
    * [feat] finish cookie and router check for unlogin user
    
    * [feat] finish cookie and router check for unlogin user
    
    ---------
    
    Co-authored-by: Lu <83503785+Eric7654321@users.noreply.github.com>
    Co-authored-by: Eric7654321 <he888hello22@gmail.com>
    Co-authored-by: ja0919a <105212631+ja0919a@users.noreply.github.com>
    Co-authored-by: Welly_Hsu <im43089@gmail.com>

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HeaderBar from './components/HeaderBar.vue';
+import WorkList from './components/WorkList.vue';
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div >
+    <HeaderBar />
+    <RouterView />
+    <!-- <WorkList /> -->
+  </div>
+</template>
+
+<style scoped>
+</style>
