2993f19bafc66c1f6ec868dc8466844ca37681b8
Welly_Hsu <im43089@gmail.com>
2025-05-21 23:26:23 +0800

    [feat] more secure login auth

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useCookies } from 'vue3-cookies';
+import { useUserData } from '@/stores/UserData';
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -9,6 +10,7 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue'),
+      meta: { requireAuth: false },
     },
     {
       path: '/employee',
@@ -17,19 +19,38 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../components/WorkList.vue'),
+      //props: route => ({ employeeId: Number(route.query.employeeId) }),
+      meta: { requireAuth: true },
     },
     {
       path: '/',
       name: 'root',
       redirect: to => {
-        const { cookies } = useCookies();
-        if(cookies.get('token') === null) {
-          return {path: 'login'}
+        if (useUserData().isAuth === false) {
+          return { path: 'login' }
         }
-        return {path: '/employee'}
+        return { path: '/employee' }
       }
     }
   ],
+})
+
+router.beforeEach((to, from) => {
+  console.log(useUserData().isAuth)
+  if (to.meta.requireAuth !== null
+    && to.meta.requireAuth === true
+    && useUserData().isAuth === false
+  ) {
+    return {
+      path: '/login',
+      query: { redirect: to.fullPath },
+    }
+  }
+  if (to.path === '/login' && useUserData().isAuth) {
+    return {
+      path: '/employee'
+    }
+  }
 })
 
 export default router
