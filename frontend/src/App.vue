97e8ebcba1781a49e1fcbe3b6810119ee684be17
welly <im43089@gmail.com>
2025-06-02 02:13:54 +0800

    fix header problem

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,27 @@ import { RouterLink, RouterView } from 'vue-router'
     <HeaderBar v-if="$route.path !== '/login'" />
     <RouterView />
   </div>
+      <HeaderBar v-if="$route.path !== '/login'" />
+      <div class="content"></div>
+      <RouterView />
 </template>
 
 <style scoped></style>
+<style>
+#app {
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 如果需要垂直居中 */
+  height: 100%; /* 仅用于演示 */
+  width: 100%;
+}
+.n-config-provider {
+  width: 100%;
+}
+</style>
+
+<style scoped>
+.content {
+  margin-top: 60px;
+}
+</style>
